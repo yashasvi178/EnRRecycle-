70197e89182dd13292938e06d5ba30d0d1aac7cb
yashasvi178 <yashasvi.thakur5001@gmail.com>
2021-10-21 16:41:01 +0530

    Updates

---
+++ precache-manifest.881cfd037d147f2b9c113990957d4d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "80b0b634b49d2d788106",
+    "url": "/EnRRecycle-/static/css/main.34a994d1.chunk.css"
+  },
+  {
+    "revision": "80b0b634b49d2d788106",
+    "url": "/EnRRecycle-/static/js/main.80b0b634.chunk.js"
+  },
+  {
+    "revision": "00e183a231893af6bfc5",
+    "url": "/EnRRecycle-/static/css/1.e840b283.chunk.css"
+  },
+  {
+    "revision": "00e183a231893af6bfc5",
+    "url": "/EnRRecycle-/static/js/1.00e183a2.chunk.js"
+  },
+  {
+    "revision": "450219a6dd42ea9a389b",
+    "url": "/EnRRecycle-/static/js/runtime~main.450219a6.js"
+  },
+  {
+    "revision": "1dc98ffe020ce6e14c9c5b90de65fac9",
+    "url": "/EnRRecycle-/index.html"
+  }
+];
